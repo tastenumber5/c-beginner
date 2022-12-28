2ac2f848b77c1c91031ba86aae920d77bc39b9a0
tastenumber5 <tastenumber5@gmail.com>
2022-12-28 17:35:18 +1030

    ValueType code along

---
+++ StudentApplication/ValueType/ValueType/Program.cs
@@ -0,0 +1,15 @@
+﻿decimal loanAmount = 0;
+decimal loanAmountCopy = 0;
+
+Console.WriteLine("Please enter a loan amount");
+
+loanAmount = Convert.ToDecimal(Console.ReadLine()); //100
+loanAmountCopy = Convert.ToDecimal(Console.ReadLine()); //50
+
+loanAmountCopy = loanAmount; //100
+
+Console.WriteLine($"loanAmount : {loanAmount} loanAmount Copy: {loanAmountCopy}");
+Console.ReadKey();
+loanAmount = loanAmount + 600;
+Console.WriteLine();
+Console.WriteLine($"After adding 600 pounds loanAmount: {loanAmount} , and loanAmountCopy: {loanAmountCopy}");
