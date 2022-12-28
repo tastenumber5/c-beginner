873124ddb3f551adb4f93d882dd3ec405fcab0e6
tastenumber5 <tastenumber5@gmail.com>
2022-12-28 17:03:53 +1030

    code

--- StudentApplication/StudentApplication/StudentApplication/Program.cs
+++ StudentApplication/StudentApplication/StudentApplication/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Collections.Concurrent;
+using System.Threading.Channels;
+using StudentApplication;
+
+int studentId = 0;
+string firstname = string.Empty;
+string lastname = string.Empty;
+decimal loanAmount = 0;
+char gender = '\0';
+bool isNew = false;
+
+Console.WriteLine("Enter your student Id");
+studentId = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your firstname");
+firstname = Console.ReadLine();
+
+Console.WriteLine("Enter your lastname");
+lastname = Console.ReadLine();
+
+string studentFullName = $"{firstname} {lastname}";
+
+Console.WriteLine($"{studentFullName} What is your current loan amount?");
+loanAmount = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine($"What is your gender ('f' female or 'm' male )");
+gender = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine($"{studentFullName} is a new student (true|false)");
+isNew = Convert.ToBoolean(Console.ReadLine());
+
+Console.Clear();
+
+Student student = new Student(studentId, firstname, lastname, loanAmount, gender, isNew);
+
+
+Console.Clear();
+
+Student studentCopy = student;
+
+Console.WriteLine("Student data  " + student.StudentData());
+
+Console.WriteLine();
+
+Console.WriteLine("Student copy data  " + studentCopy.StudentData());
+
+Console.WriteLine();
+
+Console.WriteLine("Please update the student's loan amount");
+
+student.UpdateLoanAmount(Convert.ToDecimal(Console.ReadLine()));
+
+string dividerText = "After loan update";
+
+Console.WriteLine(new String('-', dividerText.Length));
+Console.WriteLine(dividerText);
+Console.WriteLine(new String('-', dividerText.Length));
+
+Console.WriteLine("Student data  " + student.StudentData());
+
+Console.WriteLine();
+
+Console.WriteLine("Student copy data  " + studentCopy.StudentData());
+
+Console.ReadKey();
+
+
+Console.ReadKey();
---
+++ StudentApplication/StudentApplication/StudentApplication/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication
+{
+    public class Student
+    {
+        public int     _studentId { get; set; }
+        public string  _firstName { get; set; }
+        public string  _lastName { get; set; }
+        public decimal _loanAmount { get; set; }
+        public char _gender { get; set; }
+        public bool _isNew { get; set; }
+
+        public Student(int studentId, string firstname, string lastname, decimal loanAmount,char  gender, bool isNew )
+        {
+            _studentId = studentId;
+            _firstName = firstname;
+            _lastName = lastname;
+            _loanAmount = loanAmount;
+            _gender = gender;
+            _isNew = isNew;
+                
+        }
+
+        public string StudentData()
+        {
+            string data = $"Student Identity number: {_studentId}, Name: {_firstName} {_lastName}, Your Loan Amount: {_loanAmount}";
+            return data;
+        }
+
+        public void UpdateLoanAmount(decimal loanAmount)
+        {
+            _loanAmount = loanAmount;
+
+        }
+    }
+}
